d2a38d481fdebde7c4f9add0b4cdb5135b9a9088
michi <chischte@gmx.ch>
2023-12-26 21:30:58 +0100

    Orange round green end

--- src/main.cpp
+++ src/main.cpp
@@ -4,39 +4,67 @@
 #define LED_PIN 8
 #define LED_COUNT 24
 
-// Adafruit_NeoPixel ring(LED_COUNT, LED_PIN, NEO_RGBW + NEO_KHZ800);
-// Adafruit_NeoPixel ring(LED_COUNT, LED_PIN, NEO_RGBW + NEO_KHZ400);
+// ------------------------------
+unsigned long runtime_secs = 5;
+// ------------------------------
+
+unsigned long time_per_led;
+unsigned long start_time;
+
 Adafruit_NeoPixel ring(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+void set_led_orange(int led_no) {
+  ring.setPixelColor(led_no, 255, 65, 0);
+  ring.show();
+}
+
+void set_led_green(int led_no) {
+  ring.setPixelColor(led_no, 0, 255, 0);
+  ring.show();
+}
+
+void calculate_time_per_led() {
+  float float_time_per_led;
+  float number_of_leds = ring.numPixels();
+  float_time_per_led = float(runtime_secs * 1000) / number_of_leds;
+  time_per_led = float_time_per_led;
+}
+
+int calculate_current_led() {
+  unsigned long time_elapsed;
+  time_elapsed = millis() - start_time;
+  unsigned int current_led = time_elapsed / time_per_led;
+  if (current_led >= ring.numPixels() - 1) {
+    current_led = ring.numPixels() - 1;
+  }
+  return current_led;
+}
+
+void set_all_led_green() {
+  for (unsigned int i = 0; i < ring.numPixels(); i++) {
+    ring.setPixelColor(i, 0, 255, 0);
+    ring.show();
+    delay(5);
+  }
+}
 
 void setup() {
   Serial.begin(9600);
-  // put your setup code here, to run once:
   ring.begin();
   ring.show();
-  ring.setBrightness(5);
-
-  Serial.println(ring.numPixels());
+  ring.setBrightness(8);
+  calculate_time_per_led();
+  start_time = millis();
+  // ring.clear();
 }
 
 void loop() {
 
-  ring.setPixelColor(0, 250, 0, 0);
-  ring.show();
+  bool round_completed = millis() - start_time > (runtime_secs * 1000);
 
-  ring.setPixelColor(1, 0, 250, 0);
-  ring.show();
-
-  ring.setPixelColor(2, 0, 0, 250);
-  ring.show();
-
-  // for (int i = 0; i < ring.numPixels(); i++) {
-  //   ring.setPixelColor(i, 255, 0, 0);
-  //   ring.show();
-  //   delay(100);
-  // }
-  // for (int i = ring.numPixels() - 1; i >= 0; i--) {
-  //   ring.setPixelColor(i, 0, 0, 0, 0);
-  //   ring.show();
-  //   // delay(200);
-  // }
+  if (!round_completed) {
+    set_led_orange(calculate_current_led());
+  } else {
+    set_all_led_green();
+  }
 }
